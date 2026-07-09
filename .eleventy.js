263699d50b1db3b4e4c30ea43efdb106c19c70fa
djoni2 <djoni2@users.noreply.github.com>
2026-07-09 19:21:05 +0100

    Switch to using Twig for template processing
    Replaced eleventyPluginTwig with the official Twig package for processing .twig files.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,21 @@
-const eleventyPluginTwig = require("@factorial/eleventy-plugin-twig");
+const Twig = require("twig");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyPluginTwig);
-  
+  // Tell Eleventy to process .twig files using the official twig package
+  eleventyConfig.addTemplateFormats("twig");
+  eleventyConfig.addExtension("twig", {
+    compile: async (inputContent, inputPath) => {
+      const template = Twig.twig({
+        data: inputContent,
+        path: inputPath,
+        async: false
+      });
+      return async (data) => {
+        return template.render(data);
+      };
+    }
+  });
+
   return {
     dir: {
       input: ".",
